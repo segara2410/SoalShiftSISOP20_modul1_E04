3b33020e6763457694ac7ca725f59069e8ae3cf4
Segara Bhagas Dagsapurwa <segara2410@gmail.com>
2020-02-21 02:10:06 +0700

    minor fix so ls can be done quietly

--- soal3/soal3a.sh
+++ soal3/soal3a.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-i=$(ls -q pdkt_kusuma_* | grep -Eo '[0-9]+' | sort -rn | head -n 1)
+i=$(ls pdkt_kusuma_* 2> /dev/null | grep -Eo '[0-9]+' | sort -rn | head -n 1)
 i=`expr $i + 1`
 wget -O pdkt_kusuma_$i https://loremflickr.com/320/240/cat --append-output wget.log >> wget.log
