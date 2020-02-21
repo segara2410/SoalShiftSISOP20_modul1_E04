8202b462e698d69702713d335678b2e90440d130
Segara Bhagas Dagsapurwa <segara2410@gmail.com>
2020-02-21 16:11:58 +0700

    minor fix on column index at soal1.sh

--- soal1/soal1.sh
+++ soal1/soal1.sh
@@ -3,12 +3,12 @@
 a=$(awk -F "\t" '{ if ($13 != "Region") data[$13] += $21 } END { for (datum in data) { print data[datum] " " datum } }' Sample-Superstore.tsv | sort -nk1 | head -n 1 | grep -oP '(?<=[0-9.] ).*')
 echo "Region dengan profit minimum : "$a
 
-b=$(awk -v a="$a" 'BEGIN{ FS="\t" } { if ( $13 == a ) { data[$10] += $21 } } END { print $10 a; for (datum in data) {	print data[datum] " " datum } }' Sample-Superstore.tsv | sort -nk1 | head -n 2 | grep -oP '(?<=[0-9.] ).*')
+b=$(awk -v a="$a" 'BEGIN{ FS="\t" } { if ( $13 == a ) { data[$11] += $21 } } END { print $11 a; for (datum in data) {	print data[datum] " " datum } }' Sample-Superstore.tsv | sort -nk1 | head -n 2 | grep -oP '(?<=[0-9.] ).*')
 b=$(echo "$b" | tr "\n" ";")
 b=${b::-1}
 echo "State dengan profit minimum : "$b
 
-c=$(awk -v b="$b" 'BEGIN{	FS="\t"; split(b, check, ";"); } { if ( check[1] == $10 || check[2] == $10 ){ data[$17] += $21 } } END { for (datum in data) { print data[datum] " " datum } }' Sample-Superstore.tsv | sort -nk1 | head -n 10 | grep -oP '(?<=[0-9.] ).*')
+c=$(awk -v b="$b" 'BEGIN{	FS="\t"; split(b, check, ";"); } { if ( check[1] == $11 || check[2] == $11 ){ data[$17] += $21 } } END { for (datum in data) { print data[datum] " " datum } }' Sample-Superstore.tsv | sort -nk1 | head -n 10 | grep -oP '(?<=[0-9.] ).*')
 c=$(echo "$c" | tr "\n" ";")
 c=${c::-1}
 echo "Produk dengan profit minimum : "$c
