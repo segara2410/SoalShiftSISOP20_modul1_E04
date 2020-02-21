8010950a25c5acbbba883b630a22c74279798399
Segara Bhagas Dagsapurwa <segara2410@gmail.com>
2020-02-21 16:24:32 +0700

    minor change on time check at soal2enkripsi.sh

--- soal2/soal2_enkripsi.sh
+++ soal2/soal2_enkripsi.sh
@@ -2,5 +2,6 @@
 
 normal='abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz'
 uppercase='ABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZ'
-new_name=$(echo "${1%.txt}" | tr ${normal:0:26}${uppercase:0:26} ${normal:`date +%-H`:26}${uppercase:`date +%-H`:26})
+last_modified=$(stat -c %y $1 | grep -oP '(?<=[^ ] ).*(?=:.*:)')
+new_name=$(echo "${1%.txt}" | tr ${normal:0:26}${uppercase:0:26} ${normal:last_modified:26}${uppercase:last_modified:26})
 cp $1 $new_name".txt"
