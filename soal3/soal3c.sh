9ad0c6fcb3cbac10b028130d4447b0079543636e
Segara Bhagas Dagsapurwa <segara2410@gmail.com>
2020-02-21 12:38:14 +0700

    minor fix on sorting at soal3c.sh

--- soal3/soal3c.sh
+++ soal3/soal3c.sh
@@ -4,9 +4,9 @@ cat wget.log | grep Location: > location.log
 
 mkdir -p duplicate kenangan
 
-awk '{ print i + 1 ";" $2; i += 1 }' location.log | sort -nk2 > tmp.log
-awk -F ';' '{ if (i != $2 ){ i = $2; cmd = " mv pdkt_kusuma_" $1 " kenangan/kenangan_" $1; }
-  else{ cmd = "mv pdkt_kusuma_" $1 " duplicate/duplicate_" $1; } system(cmd); }' tmp.log
+awk '{ printf("%s %03d\n", $2, i + 1); i += 1 }' location.log | sort -n -k1 > tmp.log
+awk -F ' ' '{ i = $2+0; if ( loc != $1 ){ loc = $1; cmd = " mv pdkt_kusuma_" i " kenangan/kenangan_" i; }
+  else{ cmd = "mv pdkt_kusuma_" i " duplicate/duplicate_" i; } system(cmd); }' tmp.log
 rm tmp.log
 
 for f in *.log; 
